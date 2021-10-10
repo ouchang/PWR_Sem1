90b6fa078ab6a1dbf43438d0ae78403953c907f0
Aleksandra Zaremba <aleksandra.zaremba02@gmail.com>
2021-10-10 21:46:14 +0200

    Add zad4 in Lista2 for labs

---
+++ Lista2/zad4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double w=1;
+    double p;
+    double st=1000.0;
+
+    for(int i=1; i<=1000; i++) {
+        p = pow(i,1.0/st); //sqrt^1000(1) * sqrt^1000(2) * ... sqrt^1000(1000) = sqrt^1000(1000!)
+        w *= p;
+    }
+
+    printf("%.2f\n", w);
+}
